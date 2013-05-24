0e3278455861884bb8a54ef19e5268970d85a9a7
Martin Peschke <martin@per-4.com>
2013-05-24 22:12:16 +0200

    first stylings

--- webapp/ufostart/website/apps/models/company.py
+++ webapp/ufostart/website/apps/models/company.py
@@ -4,17 +4,20 @@ from hnc.apiclient.backend import ClientTokenProc
 
 
 class NeedModel(Mapping):
+    key = TextField()
     name = TextField()
 
 
 class TemplateModel(Mapping):
+    key = TextField()
     name = TextField()
     Need = ListField(DictField(NeedModel))
 
-class TemplateListModel(Mapping):
-    name = TextField()
+    def getStyleKey(self):
+        return self.key.lower()
 
-GetAllCompanyTemplatesProc = ClientTokenProc("/web/template/list", result_cls=TemplateListModel, root_key="Templates")
+
+GetAllCompanyTemplatesProc = ClientTokenProc("/web/template/list", result_cls=TemplateModel, root_key="Templates", result_list=True)
 GetTemplateDetailsProc = ClientTokenProc("/web/template", result_cls=TemplateModel, root_key="Template")
 GetAllNeedsProc = ClientTokenProc("/web/need/list", result_cls=NeedModel, root_key="Needs", result_list=True)
 
