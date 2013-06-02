4af8df276c6a53d948da12b5592bdd79e1fceb40
Martin Peschke <martin@per-4.com>
2013-06-02 23:02:42 +0200

    base round page

--- webapp/ufostart/website/apps/company/general.py
+++ webapp/ufostart/website/apps/company/general.py
@@ -3,4 +3,4 @@ from ufostart.website.apps.models.company import GetCompanyProc
 
 def index(context, request):
     company = GetCompanyProc(request, {'token': context.user.Company.token})
-    return {'company': company}
+    return {'company': company, 'hasRounds': len(company.Rounds) > 0}
