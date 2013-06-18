566fd0caf8fab8716f1678f698dc3565e4085dd2
Martin Peschke <martin@per-4.com>
2013-06-18 11:58:13 +0200

    refined picture uploader

--- webapp/ufostart/website/static/scripts/libs/fileupload.js
+++ webapp/ufostart/website/static/scripts/libs/fileupload.js
@@ -5,17 +5,13 @@
             var view = this, uploader = this.uploader, picture_template = _.template(this.$(".picture-template").html());
             filepicker.makeDropPane(uploader[0], {
                 dragEnter: function() {
-                    uploader.html('<div class="info">Drop to upload</div>').css({
-                        'backgroundColor': "rgba(0,0,0,.1)"
-                    });
+                    uploader.addClass("drop-target");
                 },
                 dragLeave: function() {
-                    uploader.html("Drop files here").css({
-                        'backgroundColor': "rgba(0,0,0,0)"
-                    });
+                    uploader.removeClass("drop-target");
                 },
                 onSuccess: function(fpfiles) {
-                    uploader.html('<div class="info">Done!</div>');
+                    uploader.html('<div class="info">Done!</div>').removeClass("empty");
                     var file = fpfiles[0];
                     uploader.html(picture_template({field_name: view.uploader.data("fieldName"), pic: fpfiles[0]}));
                 },
