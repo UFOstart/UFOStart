6d859b02e0236eeba7d8d066f4bf34d104d920d5
Martin Peschke <martin@per-4.com>
2013-06-25 19:01:58 +0200

    adding more

--- webapp/ufostart/website/apps/company/need.py
+++ webapp/ufostart/website/apps/company/need.py
@@ -1,8 +1,8 @@
 from hnc.apiclient.backend import DBNotification
-from hnc.forms.formfields import BaseForm, StringField, REQUIRED, TextareaField, TagSearchField, IntField, HtmlAttrs, HORIZONTAL_GRID, DecimalField
+from hnc.forms.formfields import BaseForm, StringField, REQUIRED, TextareaField, IntField, HtmlAttrs, HORIZONTAL_GRID, DecimalField
 from hnc.forms.handlers import FormHandler
 from hnc.forms.messages import GenericSuccessMessage
-from ufostart.website.apps.forms.controls import FileUploadField
+from ufostart.website.apps.forms.controls import FileUploadField, TagSearchField
 from ufostart.website.apps.auth.social import AuthedFormHandler
 from ufostart.website.apps.models.procs import CreateNeedProc, EditNeedProc, ApplyForNeedProc
 
--- webapp/ufostart/website/apps/forms/controls.py
+++ webapp/ufostart/website/apps/forms/controls.py
@@ -1,14 +1,16 @@
-from hnc.forms.formfields import NullConfigModel, StringField
+from hnc.forms import formfields
 from ufostart.models.tasks import TASK_CATEGORIES
 
 
 def optionGetter(request):
-    return [NullConfigModel()] + TASK_CATEGORIES
+    return [formfields.NullConfigModel()] + TASK_CATEGORIES
 
 
-class FileUploadField(StringField):
+class FileUploadField(formfields.StringField):
     template = "ufostart:website/templates/company/controls/fileupload.html"
     group_classes='file-upload-control'
 
 
 
+class TagSearchField(formfields.TagSearchField):
+    template = "ufostart:website/templates/company/controls/tagsearch.html"
